1f6a26a2c30854d1ac37b1d9658ed50280f2d07d
MinhPhu <minhphuzcod@gmail.com>
2023-12-13 16:06:36 +0700

    13/12/2023

--- Datawarehouse/src/model/Log.java
+++ Datawarehouse/src/model/Log.java
@@ -2,6 +2,7 @@ package model;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 
@@ -13,7 +14,7 @@ import controller.LoadFromTempToDataWareHouse;
 public class Log {
 
 	private static Config cof = new Config();
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(LoadFromTempToDataWareHouse.class);
 	private static final String LOG_INSERT_QUERY = "INSERT INTO Log (Time, Title, Description, Config_Id, Status) VALUES (?, ?, ?, ?, ?)";
 
@@ -27,14 +28,14 @@ public class Log {
 			insertLogStatement.setInt(4, configId);
 			insertLogStatement.setInt(5, status);
 
-			insertLogStatement.executeUpdate();			
+			insertLogStatement.executeUpdate();
 		} catch (SQLException e) {
 			System.err.println("Error inserting log: " + e.getMessage());
 		}
 	}
 
 	public Connection getControlConnection() throws SQLException {
-		return cof.getConnectionFromProperties("control");
+		return cof.connectToDatabase("control");
 	}
 
 	public void logInfo(String message) {
@@ -43,6 +44,27 @@ public class Log {
 
 	public void logError(String message, Throwable throwable) {
 		logger.error(message, throwable);
+	}
+
+	public boolean latestLogHasStatus() {
+		try (Connection dataWarehouseConnection = cof.connectToDatabase("control")) {
+			String query = "SELECT l.Status FROM Log l ORDER BY l.Time DESC LIMIT 1";
+			try (PreparedStatement preparedStatement = dataWarehouseConnection.prepareStatement(query);
+					ResultSet resultSet = preparedStatement.executeQuery()) {
+				if (resultSet.next()) {
+					int latestStatus = resultSet.getInt("Status");
+					logInfo("Latest Log Status: " + latestStatus);
+
+					return latestStatus == 7|| latestStatus == 10;
+				} else {
+					logInfo("No log entries found.");
+					return false;
+				}
+			}
+		} catch (SQLException e) {
+			logError("Error while checking latest log status: " + e.getMessage(), e);
+			return false;
+		}
 	}
 
 }
--- Datawarehouse/src/model/Main.java
+++ Datawarehouse/src/model/Main.java
@@ -1,12 +1,17 @@
 package model;
 
 import controller.LoadFromTempToDataWareHouse;
+import controller.LoadFromDataWareHousetoDataMart;
 
 public class Main {
 
 	private static LoadFromTempToDataWareHouse l1 = new LoadFromTempToDataWareHouse();
+	private static LoadFromDataWareHousetoDataMart l2 = new LoadFromDataWareHousetoDataMart();
+	private static Config cof = new Config();
 
 	public static void main(String[] args) {
 		l1.loadDataFromTempToDataWarehouse();
+//		l2.LoadDatawarehouseToDataMart();
+//		cof.loadConfigToDatabase("control");
 	}
 }
